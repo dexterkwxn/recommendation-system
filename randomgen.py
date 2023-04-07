b0dd4e2fbcd907a8e733d96847bbe80687128d77
dizur <darion.kwan@yahoo.com.sg>
2023-04-07 21:59:33 +0800

    y

--- randomgen.py
+++ randomgen.py
@@ -8,5 +8,10 @@ cost_list = [[300,330,350,380,400,410],[450,480,510,550,580,600]]
 
 def generate_room_type():
     room = []  
-    room.append(sd_list[random.randint(0,8)])
-    
+    room.append(sd_list[random.randint(0,7)])
+    if "Single" in room[0]:
+        room.append(cost_list[1][random.randint(0,5)])
+    else:
+        room.append(cost_list[0][random.randint(0,5)])
+
+    return room
