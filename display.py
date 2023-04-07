c461c304358fef68c0936e5cfa0954897c8c9610
dizur <darion.kwan@yahoo.com.sg>
2023-04-07 22:58:14 +0800

    y

--- display.py
+++ display.py
@@ -29,34 +29,22 @@ def recommendation_menu_text():
     print('Welcome to the recommendation menu.')
 
 def req_budget_text():
-    print('Input your estimated budget')
+    print('Input your estimated budget (eg. 350)')
 
 def req_school_text():
     print('Attached are the schools in NTU, input the index number of the school from which your course is from')
-    print('''1. Nanyang Business School (NBS)
-2. School of Chemistry, Chemical Engineering and Biotechnology (CCEB)
-3. School of Electrical and Electronic Engineering (EEE)
-4. School of Civil and Environmental Engineering (CEE)
-5. School of Materials Science and Engineering (MSE)
-6. School of Computer Science and Engineering (SCSE)
-7. School of Mechanical and Aerospace Engineering (MAE)
-8. School of Humanities (SOH)
-9. School of Art, Design and Media (ADM)
-10. Wee Kim Wee School of Communication and Information (WKWSCI)
-11. School of Social Sciences (SSS)
-12. School of Physical and Mathematical Sciences (SPMS)
-13. School of Biological Sciences (SBS)
-14. The Asian School of the Environment (ASE)
-15. Lee Kong Chian School of Medicine
-16. National Institute of Education (NIE), Singapore
-17. S. Rajaratnam School of International Studies (RSIS)''')
-
+    for i, name in school_mapping:
+        print(i, ": ", name)
 
 def req_roomtype_text():
-    print('Input your desired room type (Single/Double)')
+    print('''Input the index of your desired room type
+1. Single
+2. Double''')
 
 def req_aircon_text():
-    print('Input choice of aircon (Y/N)')
+    print('''Input the index of choice of aircon
+1. No AC
+2. AC''')
 
 def req_facilities_text():
     print('Attached are facilities near the halls, input the index option')
